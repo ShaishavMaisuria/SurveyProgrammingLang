092073c87edf6b12911b0bae41d50cbfe027efd3
ShaishavMaisuria <smaisuri@uncc.edu>
2022-04-10 14:09:30 -0400

    Removed bugs related to course objects inside each student, appointed my own gpa logic

--- finalProject.go
+++ finalProject.go
@@ -47,43 +47,46 @@ func main() {
 	var studentArray []student
 	fileName = "data.txt"
 	studentArray = read_data(fileName)
-	var courseArray [2]course
-	for i, s := range studentArray {
+	// var courseArray [2]course
+	// for i, s := range studentArray {
 
-		fmt.Println(i, s)
-		fmt.Println(" Please enter the data for the following student id", s.id)
-		courseArray = enterCourseInformation()
-		for i, s := range studentArray {
-			fmt.Println(i)
-			var n = 2
-			var j int
-			for j = 0; j < n; j++ {
-				s.courses[j].courseName = courseArray[j].courseName
+	// 	fmt.Println(i, s)
+	// 	fmt.Println(" Please enter the data for the following student id", s.id)
+	// 	courseArray = enterCourseInformation()
+	// 	for i, s := range studentArray {
+	// 		fmt.Println(i)
+	// 		var n = 2
+	// 		var j int
+	// 		for j = 0; j < n; j++ {
+	// 			s.courses[j].courseName = courseArray[j].courseName
 
-				s.courses[j].gradeLetter = courseArray[j].gradeLetter
-				s.courses[j].studentgrades = courseArray[j].studentgrades
-				s.courses[j].instructorName = courseArray[j].instructorName
-				s.courses[j].credit = courseArray[j].credit
+	// 			s.courses[j].gradeLetter = courseArray[j].gradeLetter
+	// 			s.courses[j].studentgrades = courseArray[j].studentgrades
+	// 			s.courses[j].instructorName = courseArray[j].instructorName
+	// 			s.courses[j].credit = courseArray[j].credit
 
-				// fmt.Println(courseArray)
-				// print_courseDetails(courseArray[j])
-				fmt.Println("courname", s.courses[j])
-			}
+	// 			// fmt.Println(courseArray)
+	// 			// print_courseDetails(courseArray[j])
+	// 			fmt.Println("courname", s.courses[j])
+	// 		}
 
-		}
+	// 	}
 
-	}
+	// }
 
 	// issue is not able to write a pointer so that values can be updated
 	for i, s := range studentArray {
+
+		fmt.Println("************************************Program Start*************************************")
 		fmt.Println(i)
-		var n = 2
-		var j int
-		for j = 0; j < n; j++ {
-			fmt.Println("structured course name:", s.courses[i].courseName)
-			print_courseDetails(s.courses[j])
-		}
-		fmt.Println("structured course name1:", s.courses)
+		// var n = 2
+		// var j int
+		// for j = 0; j < n; j++ {
+		// 	fmt.Println("structured course name:", s.courses[i].courseName)
+		// 	print_courseDetails(s.courses[j])
+		// }
+		print_StudentInformation(s)
+		// fmt.Println("structured course name1:", s.courses)
 
 	}
 
@@ -142,73 +145,54 @@ func enterCourseInformation() [2]course {
 }
 
 //implement grade and write code accordingly TODO
-func findGradeLetter(sObject student) {
+func findGradeLetter(cObject [2]course) float64 {
 
 	// refference change variable nameshttps://stackoverflow.com/questions/59623663/trouble-creating-gpa-calculator
 
 	totalCredits := 0.0
 	totalScore := 0.0
 
-	var subjects int
-	for {
-		fmt.Println("Enter number of subjects:")
-		n, err := fmt.Scan(&subjects)
-		if n == 1 && err == nil {
-			if subjects >= 0 && subjects <= 20 {
-				break
-			}
-		}
-	}
-
-	for i := 0; i < subjects; i++ {
-		var grade float64
-		for {
-			var studentletterGrade string
-			fmt.Println("Enter letter grade:")
-			n, err := fmt.Scanf("%s", &studentletterGrade)
-			if n == 1 && err == nil {
-				var ok bool
-				grade, ok = grades[strings.ToUpper(studentletterGrade)]
-				if ok {
-					break
-				}
-			}
-		}
-
-		var credits float64
-		for {
-			fmt.Println("Enter credits:")
-			n, err := fmt.Scan(&credits)
-			if n == 1 && err == nil {
-				if credits >= 0 && credits <= 10 {
-					break
-				}
-			}
-		}
+	// var subjects int = 2
+	var grade float64
+	var credits float64
+	for i := 0; i < len(cObject); i++ {
+		grade = grades[strings.ToUpper(cObject[i].gradeLetter)]
+		credits = cObject[i].credit
 
 		totalCredits += credits
 		totalScore += credits * grade
-	}
 
+	}
 	gpa := 0.0
 	if totalCredits != 0.0 {
 		gpa = totalScore / totalCredits
 	}
 	fmt.Println("GPA:", gpa)
-
+	return gpa
 }
 
 // printing the course information
 func print_courseDetails(sObject course) {
-	fmt.Printf("For the following course name : %s, taught by %s, student has current lettergrade %s and grade in the class is %d %", sObject.courseName, sObject.instructorName, sObject.gradeLetter, sObject.studentgrades)
+	fmt.Printf("For the following course name : %s, taught by %s, student has current lettergrade %s and grade in the class is %f ", sObject.courseName, sObject.instructorName, sObject.gradeLetter, sObject.studentgrades)
 }
 
 // in the go we declare function like below fun functionName (parameter parameter_type )
 func print_StudentInformation(sObject student) {
+	fmt.Println("------------------------Student Information-------------------------------")
 	fmt.Printf("%s %s whos 800 number is %d , and is student at College %s at the age of %d", sObject.firstName, sObject.lastName, sObject.id, sObject.college, sObject.age)
+	fmt.Print("///Course Information////")
+	var n = 2
+	var j int
+	for j = 0; j < n; j++ {
+		fmt.Println("structured course name:", sObject.courses[j].courseName)
+		print_courseDetails(sObject.courses[j])
+	}
+
 }
 
 func read_data(file string) []student {
+
+	fmt.Println("This Program is to demonstrate the Input and output Operations in Go Language ")
 	f, errorVal := os.Open(file)
 
 	if errorVal != nil {
@@ -220,19 +204,32 @@ func read_data(file string) []student {
 	scanner := bufio.NewScanner(f)
 	var str = ""
 	var arraylist []student
-
+	// var studentGPA float64
 	for scanner.Scan() {
 
 		str = scanner.Text()
 		// fmt.Println()
 		split := strings.Split(str, ",")
 		// fmt.Print("vale", split[0])
-		arraylist = append(arraylist, student{id: split[0], firstName: split[1], lastName: split[2], birthYear: split[3], college: split[4]})
+
+		fmt.Println("********************************************************************************")
+
+		courseArray := enterCourseInformation()
+		studentGPA := findGradeLetter(courseArray)
+		arraylist = append(arraylist, student{id: split[0], firstName: split[1], lastName: split[2], birthYear: split[3], college: split[4], courses: courseArray, gpa: studentGPA})
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	// var j int
+	// for j = 0; j < n; j++ {
+	// 	fmt.Println("-----------------------------------------------")
+
+	// 	fmt.Println("Student" + strconv.Itoa(j+1))
+	// 	print_courseDetails(courseArray[j])
+	// 	// fmt.Println()
+	// }
 
 	fmt.Print("arayList", arraylist)
 	return arraylist
